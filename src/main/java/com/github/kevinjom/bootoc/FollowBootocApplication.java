a482d1c30e25daa46f93cce2c364184b4a738805
kevinjom <gokevinvan@gmail.com>
2016-04-18 23:21:33 +0800

    [#23] -  Customizing SpringApplication
    1. add `bootoc` banner
    2. use the fulent builder api to build a `SpringApplication`

--- src/main/java/com/github/kevinjom/bootoc/FollowBootocApplication.java
+++ src/main/java/com/github/kevinjom/bootoc/FollowBootocApplication.java
@@ -1,12 +1,19 @@
 package com.github.kevinjom.bootoc;
 
+import org.springframework.boot.Banner;
+import org.springframework.boot.ResourceBanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 public class FollowBootocApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FollowBootocApplication.class, args);
+        new SpringApplicationBuilder(FollowBootocApplication.class)
+                .bannerMode(Banner.Mode.CONSOLE)
+                .banner(new ResourceBanner(new ClassPathResource("banner.txt")))
+                .run(args);
     }
 }
